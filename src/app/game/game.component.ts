60505739982353c5b7b842c3f4f8e9cd19b4bdbf
deniseS85 <denisabel78@hotmail.de>
2023-04-11 12:30:53 +0200

    added player, added gamerules

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 
 @Component({
@@ -12,6 +14,7 @@ export class GameComponent implements OnInit {
   currentCard: string = '';
   game: Game;
 
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit() {
     this.newGame();
@@ -19,11 +22,30 @@ export class GameComponent implements OnInit {
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
   }
 
   takeCard() {
-    /* this.currentCard = this.game.cards.pop(); */
-    this.takeCardAnimation = true;
+    if (!this.takeCardAnimation) {
+      this.currentCard = this.game.cards.pop();
+      this.takeCardAnimation = true;  
+     
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.player.length;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.takeCardAnimation = false;
+      }, 1200);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(name => {
+      if (name && name.length > 0) {
+        this.game.player.push(name);
+      }
+    });
   }
 }
